983dd40c8e583761a6eb1664fb4368c9675fd11e
mirkodandrea <mirko.dandrea@gmail.com>
2022-12-18 18:30:26 +0100

    day 6

---
+++ aoc-06b/src/main.rs
@@ -0,0 +1,17 @@
+const N_CHARS: usize = 14;
+
+fn main() {
+    let input = include_str!("../input.txt");
+    (N_CHARS..=input.len())
+        .take_while(|i| {
+            let i = *i;
+            let mut chars: Vec<char> = input[i - N_CHARS..i].chars().collect::<Vec<char>>();
+
+            chars.sort();
+            chars.dedup();
+
+            chars.len() < N_CHARS
+        })
+        .last()
+        .map(|i| println!("{}", i + 1));
+}
